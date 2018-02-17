a508cab18c2e45c1015a4bd18c37bf612bdeaffc
Shannon Beach <shannonbeach1982@gmail.com>
2018-02-17 12:46:41 -0500

    question 2 basic layout and logic working

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,13 +3,17 @@ package com.example.android.quizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     int score = 0;
     int question_one_points = 0;
+    int question_two_points = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    /**
+        get the user's answer, compare it to the correct answer
+     */
     public int question_one_points(){
         String correct_answer = "Scoobert Doo";
 
@@ -24,16 +31,35 @@ public class MainActivity extends AppCompatActivity {
         String user_answer =  user_answered.getText().toString();
 
         if(user_answer.equals(correct_answer)){
-            question_one_points += 2 ;
-            return question_one_points;
+            return question_one_points += 2 ;
         }
 
         return question_one_points;
-
     }
+
+    /*
+        Determine if right radio button chosen for question 2
+     */
+
+    public int getQuestion_two_points() {
+
+        RadioButton mysteryMachineButton = (RadioButton) findViewById(R.id.question_two_correct_answer);
+        boolean isMysteryMachine = mysteryMachineButton.isChecked();
+
+        if(isMysteryMachine){
+            return question_two_points += 2;
+//           / Toast.makeText(this, "Only one answer can be chosen", Toast.LENGTH_SHORT).show();
+
+        }
+
+        return question_two_points;
+    }
+
+
 
     public int calculateScore(){
         score += question_one_points();
+        score += getQuestion_two_points();
         return score;
 
     }
